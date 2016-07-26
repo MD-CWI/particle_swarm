MD-CWI/particle_swarm

commit c92b0106f1bff55a0c5a8cd75a3f006f374517b4

Add comments

src/m_particle_swarm.f90
@@ -251,7 +251,11 @@ contains
     ! Loop over the swarms until converged
     do while (.not. accurate .or. n_swarms < n_swarms_min)
 
-       n_coll_times = nint(fac * ps%v2 / abs(ps%fld * ps%v(3)) / tau_coll)
+       ! Estimate the time scale for energy relaxation, given by:
+       ! energy / (d/dt energy), in units of the collision time.
+       n_coll_times = nint(fac * ps%v2 / abs(ps%fld * ps%v(3) * tau_coll)
+
+       ! For safety, limit n_coll_times to 10 -- 2000
        n_coll_times = max(10, n_coll_times)
        n_coll_times = min(2000, n_coll_times)
 

commit 3e12d71605848857b123d45909735982f7cf899e

Undo typo introduced in c92b010

src/m_particle_swarm.f90
@@ -253,7 +253,7 @@ contains
 
        ! Estimate the time scale for energy relaxation, given by:
        ! energy / (d/dt energy), in units of the collision time.
-       n_coll_times = nint(fac * ps%v2 / abs(ps%fld * ps%v(3) * tau_coll)
+       n_coll_times = nint(fac * ps%v2 / abs(ps%fld * ps%v(3)) * tau_coll)
 
        ! For safety, limit n_coll_times to 10 -- 2000
        n_coll_times = max(10, n_coll_times)