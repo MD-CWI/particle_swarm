24cdd206d99391bfbcc4479daee7f8ff876d1f27
Jannis Teunissen <jannis@teunissen.net>
2024-02-28 15:55:10 +0100

    Add estimate of mean free path

--- tools/plot_relaxation_times.py
+++ tools/plot_relaxation_times.py
@@ -22,7 +22,8 @@ args = parser.parse_args()
 
 df = pd.read_csv(args.in_file)
 
-fig, ax = plt.subplots(figsize=(6, 4), layout='constrained')
+fig, ax = plt.subplots(2, figsize=(6, 8), layout='constrained',
+                       sharex=True)
 
 x = 'E (MV/m)'
 df[x] = df['Electric field (V/m)'] * 1e-6
@@ -35,17 +36,31 @@ df['tau ionization'] = 1 / (np.abs(df['Townsend ioniz. coef. alpha (1/m)'] -
                             df['Mobility (m2/V/s)'] *
                             df['Electric field (V/m)'])
 
-ax.plot(df[x], df['tau energy'], label=r'$\tau_\varepsilon$')
-ax.plot(df[x], df['tau coll'], label=r'$\tau_\mathrm{coll}$')
-ax.plot(df[x], df['tau ionization'], label=r'$\tau_\alpha$')
+# This is only an estimate
+df['v norm'] = np.sqrt(df['Velocity squared x (m2/s2)'] +
+                       df['Velocity squared y (m2/s2)'] +
+                       df['Velocity squared z (m2/s2)'])
 
-ax.set_xlabel(x)
-ax.set_ylabel('t (s)')
-ax.legend()
-ax.set_yscale('log')
+# Temporal scales
+ax[0].plot(df[x], df['tau energy'], label=r'$\tau_\varepsilon$')
+ax[0].plot(df[x], df['tau coll'], label=r'$\tau_\mathrm{coll}$')
+ax[0].plot(df[x], df['tau ionization'], label=r'$\tau_\alpha$')
+
+ax[0].set_xlabel(x)
+ax[0].set_ylabel('t (s)')
+ax[0].legend()
+ax[0].set_yscale('log')
 
 if args.ymax:
-    ax.set_ylim(None, args.ymax)
+    ax[0].set_ylim(None, args.ymax)
+
+# Spatial scales
+ax[1].plot(df[x], df['tau coll'] * df['v norm'],
+           label=r'$\tau_\mathrm{coll} \sqrt{v^2}$')
+ax[1].set_xlabel(x)
+ax[1].set_ylabel('length (m)')
+ax[1].legend()
+ax[1].set_yscale('log')
 
 if args.savefig:
     plt.savefig(args.savefig, dpi=300, bbox_inches='tight')
