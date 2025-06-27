4316bd2ce224b212db478af139f28213119de723
Jannis Teunissen <jannis@teunissen.net>
2025-06-27 14:37:48 +0200

    Update plot script

--- tools/plot_swarm_data.py
+++ tools/plot_swarm_data.py
@@ -13,7 +13,8 @@ parser = argparse.ArgumentParser(
     description='''Plot swarm data''')
 parser.add_argument('in_file', type=argparse.FileType('r'),
                     help='CSV file with output from swarm_cli.py')
-parser.add_argument('parameter', type=str, default='Mean energy (eV)',
+parser.add_argument('parameter', type=str, nargs='+',
+                    default=['Mean energy (eV)'],
                     help='Which quantity to plot')
 parser.add_argument('-x', type=str, default=['Electric field / N (Td)'],
                     help='Use this as x-axis')
@@ -23,17 +24,18 @@ df = pd.read_csv(args.in_file)
 
 parameters = df.columns.values
 x = args.x
-y = args.parameter
 
 if x not in parameters:
     x = get_close_matches(x, parameters, cutoff=0.1)[0]
     print(f'Assuming you mean "{args.x}" = "{x}"')
-if y not in parameters:
-    y = get_close_matches(y, parameters, cutoff=0.1)[0]
-    print(f'Assuming you mean "{args.parameter}" = "{y}"')
 
 df.set_index(x, inplace=True)
 
-df[y].plot()
+for y in args.parameter:
+    if y not in parameters:
+        y = get_close_matches(y, parameters, cutoff=0.1)[0]
+        print(f'Assuming you mean "{args.parameter}" = "{y}"')
+    df[y].plot()
+
 plt.legend()
 plt.show()
